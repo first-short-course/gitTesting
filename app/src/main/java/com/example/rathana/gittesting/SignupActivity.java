4f96ad43e5e256d13661f35e31ebf53250f65d31
rathana-voy <rathanavoy07@gmail.com>
2018-06-23 16:21:27 +0700

    add some code to signup screen

--- app/src/main/java/com/example/rathana/gittesting/SignupActivity.java
+++ app/src/main/java/com/example/rathana/gittesting/SignupActivity.java
@@ -2,6 +2,7 @@ package com.example.rathana.gittesting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class SignupActivity extends AppCompatActivity {
 
@@ -9,5 +10,9 @@ public class SignupActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        for(int i=0;i<20;i++){
+            Log.e("Sihnup"," "+i);
+        }
     }
 }
